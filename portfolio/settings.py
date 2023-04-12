7a42e0df2a66157f4fd408971258584512be9d17
Veronica Jones <veronicastarr93@gmail.com>
2023-04-12 11:33:07 -0500

    typo boto - boto3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -108,7 +108,7 @@ else:
 # Django-Storages
 if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
