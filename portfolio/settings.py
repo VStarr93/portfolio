99ee0f531cd5ed1000bf77a8034e758ac9ac55e1
Veronica Jones <veronicastarr93@gmail.com>
2023-04-15 15:04:55 -0500

    add Email Settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -162,3 +162,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # User substitution
 # https://docs.djangoproject.com/en/1.11/topics/auth/customizing/#auth-custom-user
 AUTH_USER_MODEL = 'crm_user.User'
+
+# Email Settings
+# https://docs.djangoproject.com/en/4.2/topics/email/
+EMAIL_BACKEND = os.environ['EMAIL_BACKEND']
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+RECIPIENT_ADDRESS = os.environ['RECIPIENT_ADDRESS']
