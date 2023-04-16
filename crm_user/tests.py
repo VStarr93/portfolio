7d26ccf93d730726f63f29209d4eac5781989ad4
Veronica Jones <veronicastarr93@gmail.com>
2023-04-16 14:00:07 -0500

    add description to each test in UserCreateTests

--- crm_user/tests.py
+++ crm_user/tests.py
@@ -14,24 +14,37 @@ from .models import User, Customer, Employee, Admin
 # Create a TestCase for User Creation
 class UserCreateTests(TestCase):
     def test_create_user_with_password(self):
+        """
+            User is created successfully with email and password.
+        """
         new_user = User.objects.create_user(email='test@example.com',password='test123')
         all_users = User.objects.all() 
         self.assertIn(new_user, all_users)
         self.assertIs(new_user.check_password('test123'), True)
     
     def test_create_user_without_password(self):
+        """
+            User is created successfully with email and no password.
+            User does not have a usable password.
+        """
         new_user = User.objects.create_user(email='test1@example.com')
         all_users = User.objects.all() 
         self.assertIn(new_user, all_users)
         self.assertIs(new_user.has_usable_password(), False)
 
     def test_create_superuser_with_password(self):
+        """
+            SuperUser is created successfully with email and password.
+        """
         new_superuser = User.objects.create_superuser(email="test2@example.com", password="test123")
         all_users = User.objects.all() 
         self.assertIn(new_superuser, all_users)
         self.assertTrue(new_superuser.has_usable_password())
         
     def test_create_superuser_without_password(self):
+        """
+            SuperUser cannot be created without a password.
+        """
         with self.assertRaises(TypeError):
             new_superuser = User.objects.create_superuser(email="test2@example.com")
 
