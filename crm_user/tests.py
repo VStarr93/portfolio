40d8fb1aa0f76cb5bf1df2bbac5e4eba88a0eb11
Veronica Jones <veronicastarr93@gmail.com>
2023-04-16 12:59:27 -0500

    organize tests.py with sectioned comments

--- crm_user/tests.py
+++ crm_user/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+#./crm_user/tests.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
 
-# Create your tests here.
+from django.test import TestCase
+from .models import User, Customer, Employee, Admin
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your User Model tests here.
+
+# Create a TestCase for User Creation
+
+# Create a TestCase for User Methods
+
+# Create a TestCase for User Field Validations
+
+# Create a TestCase for User Permissions
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Customer Model tests here.
+
+# Create a TestCase for Customer Creation
+
+# Create a TestCase for Customer Methods
+
+# Create a TestCase for Customer Field Validations
+
+# Create a TestCase for Customer Permissions
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Employee Model tests here.
+
+# Create a TestCase for Employee Creation
+
+# Create a TestCase for Employee Methods
+
+# Create a TestCase for Employee Field Validations
+
+# Create a TestCase for Employee Permissions
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Admin Model tests here.
+
+# Create a TestCase for Admin Creation
+
+# Create a TestCase for Admin Methods
+
+# Create a TestCase for Admin Field Validations
+
+# Create a TestCase for Admin Permissions
+
+
