522108e1bf30dda7be5e27c3f23feeb1b3ae4259
Veronica Jones <veronicastarr93@gmail.com>
2023-04-18 12:01:31 -0500

    registered User, Customer, Employee, Admin models

--- crm_user/admin.py
+++ crm_user/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+# ./crm_user/admin.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
 
+from django.contrib import admin
+from crm_user.models import User, Customer, Employee, Admin
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Employee)
+admin.site.register(Admin)
