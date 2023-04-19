39b4e592246621eb40e7f6bbc5f72ad652e734d8
Veronica Jones <veronicastarr93@gmail.com>
2023-04-19 13:29:58 -0500

    create signal handler: create_customer_profile

---
+++ crm_user/signals/handlers.py
@@ -0,0 +1,21 @@
+# crm_user/signals/handlers.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.db.models.signals import post_save 
+from django.dispatch import receiver 
+from crm_user.models import Customer, Employee, Admin, CustomerProfile
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your handlers here.
+
+@receiver(post_save, sender=Customer)
+def create_customer_profile(sender, **kwargs):
+    user = kwargs['instance']
+    new_profile = CustomerProfile.objects.create(
+        user = user,
+        last_modified_by = user,
+    )
