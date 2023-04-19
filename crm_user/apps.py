75e16990d10d7bab6185764602cac5da19dccf77
Veronica Jones <veronicastarr93@gmail.com>
2023-04-19 13:30:34 -0500

    register signals to crm_user app

--- crm_user/apps.py
+++ crm_user/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CrmUserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crm_user'
+
+    def ready(self):
+        import crm_user.signals.handlers
