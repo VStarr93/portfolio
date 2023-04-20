4652048b924f7bb7d15c4ca2bc8277b0bc17e794
Veronica Jones <veronicastarr93@gmail.com>
2023-04-20 10:40:00 -0500

    create create_employee_profile receiver handler

--- crm_user/signals/handlers.py
+++ crm_user/signals/handlers.py
@@ -5,7 +5,7 @@
 
 from django.db.models.signals import post_save 
 from django.dispatch import receiver 
-from crm_user.models import Customer, Employee, Admin, CustomerProfile
+from crm_user.models import Customer, Employee, Admin, CustomerProfile, EmployeeProfile
 
 
 #-------------------------------------------------------------
@@ -18,6 +18,16 @@ def create_customer_profile(sender, **kwargs):
     created = kwargs['created']
     if created :
         new_profile = CustomerProfile.objects.create(
+            user = user,
+            last_modified_by = user,
+        )
+
+@receiver(post_save, sender=Employee)
+def create_employee_profile(sender, **kwargs):
+    user = kwargs['instance']
+    created = kwargs['created']
+    if created :
+        new_profile = EmployeeProfile.objects.create(
             user = user,
             last_modified_by = user,
         )
