2c25bac25319842ddeafbe3abcee234557a1ed59
Veronica Jones <veronicastarr93@gmail.com>
2023-04-20 12:54:30 -0500

    create index view for crm_user

--- crm_user/views.py
+++ crm_user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# crm_user/views.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
 
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
 # Create your views here.
+
+# INDEX view
+def index(request):
+    """
+        This is a basic view for crm_user app.
+    """
+    return HttpResponse("This is the Index for CRM_USER app.")
