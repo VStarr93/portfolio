9b8862de8115ea020a70a755c8f7cd447c826b36
Veronica Jones <veronicastarr93@gmail.com>
2023-04-20 11:15:34 -0500

    create create_admin_profile receiver handler

--- crm_user/signals/handlers.py
+++ crm_user/signals/handlers.py
@@ -5,7 +5,7 @@
 
 from django.db.models.signals import post_save 
 from django.dispatch import receiver 
-from crm_user.models import Customer, Employee, Admin, CustomerProfile, EmployeeProfile
+from crm_user.models import Customer, Employee, Admin, CustomerProfile, EmployeeProfile, AdminProfile
 
 
 #-------------------------------------------------------------
@@ -30,6 +30,17 @@ def create_employee_profile(sender, **kwargs):
     created = kwargs['created']
     if created :
         new_profile = EmployeeProfile.objects.create(
+            user = user,
+            last_modified_by = user,
+        )
+
+# Add Receiver for creating Admin Profile
+@receiver(post_save, sender=Admin)
+def create_admin_profile(sender, **kwargs):
+    user = kwargs['instance']
+    created = kwargs['created']
+    if created :
+        new_profile = AdminProfile.objects.create(
             user = user,
             last_modified_by = user,
         )
