52589583cd7b365dc6f50cddaece1cd4a957dcb3
Veronica Jones <veronicastarr93@gmail.com>
2023-04-22 09:50:44 -0500

    create CustomUserCreationForm

--- crm_user/forms.py
+++ crm_user/forms.py
@@ -5,9 +5,18 @@
 
 from django import forms 
 from crm_user.models import *
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Forms
 
+class CustomUserCreationForm(UserCreationForm):
+    """
+        Define a Custom UserCreationForm.
+    """
+
+    class Meta:
+        model = get_user_model() 
+        fields = ('first_name', 'last_name', 'type', 'email', 'password1', 'password2', )
