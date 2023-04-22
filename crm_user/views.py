7d1b1e74fc0c2c392c645c8b51861fb8e93ece67
Veronica Jones <veronicastarr93@gmail.com>
2023-04-22 09:53:00 -0500

    create user_registration view

--- crm_user/views.py
+++ crm_user/views.py
@@ -3,8 +3,10 @@
 #-------------------------------------------------------------
 # IMPORTS
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse 
+from django.contrib.auth.forms import UserCreationForm
+from crm_user.forms import *
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
@@ -16,3 +18,25 @@ def index(request):
         This is a basic view for crm_user app.
     """
     return HttpResponse("This is the Index for CRM_USER app.")
+
+# User Registration View
+def user_registration(request):
+    """
+        This is a user registration view.
+    """
+    if request.method == 'GET':
+        form = CustomUserCreationForm
+        return render(request, 'registration/user_registration.html', context={'form':form})
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/login/')
+        else:
+            message = form.errors
+            context = {
+                'form': CustomUserCreationForm(request.POST),
+                'message': message,
+            }
+            return render(request, 'registration/user_registration.html', context)
