b4a9bdbafa0a430a6f5a3240c61d7e927a3b67cc
Veronica Jones <veronicastarr93@gmail.com>
2023-04-26 15:41:13 -0500

    add create_profile receiver handler

--- crm_user/signals/handlers.py
+++ crm_user/signals/handlers.py
@@ -5,14 +5,14 @@
 
 from django.db.models.signals import post_save 
 from django.dispatch import receiver 
-from crm_user.models import Customer, Employee, Admin, CustomerProfile, EmployeeProfile, AdminProfile
+from crm_user.models import User, Customer, Employee, Admin, CustomerProfile, EmployeeProfile, AdminProfile
 
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your handlers here.
 
-# Add Receiver for creating Customer Profile
+# Add Receiver for creating Customer Profile from Customer Model
 @receiver(post_save, sender=Customer)
 def create_customer_profile(sender, **kwargs):
     user = kwargs['instance']
@@ -23,7 +23,7 @@ def create_customer_profile(sender, **kwargs):
             last_modified_by = user,
         )
 
-# Add Receiver for creating Employee Profile
+# Add Receiver for creating Employee Profile from Employee Model
 @receiver(post_save, sender=Employee)
 def create_employee_profile(sender, **kwargs):
     user = kwargs['instance']
@@ -34,12 +34,33 @@ def create_employee_profile(sender, **kwargs):
             last_modified_by = user,
         )
 
-# Add Receiver for creating Admin Profile
+# Add Receiver for creating Admin Profile from Admin Model
 @receiver(post_save, sender=Admin)
 def create_admin_profile(sender, **kwargs):
     user = kwargs['instance']
     created = kwargs['created']
     if created :
+        new_profile = AdminProfile.objects.create(
+            user = user,
+            last_modified_by = user,
+        )
+        
+# Add Receiver for creating Profiles from User Model
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    user = kwargs['instance']
+    created = kwargs['created']
+    if created and user.type == "CUSTOMER":
+        new_profile = CustomerProfile.objects.create(
+            user = user,
+            last_modified_by = user,
+        )
+    elif created and user.type == "EMPLOYEE":
+        new_profile = EmployeeProfile.objects.create(
+            user = user,
+            last_modified_by = user,
+        )
+    elif created and user.type == "ADMIN":
         new_profile = AdminProfile.objects.create(
             user = user,
             last_modified_by = user,
