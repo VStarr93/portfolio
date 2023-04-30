09f54d38f69440e704ced9f822110f1683bbfcbc
Veronica Jones <veronicastarr93@gmail.com>
2023-04-30 16:55:31 -0500

    add imports to main/models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+#./main/models.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
 
+from django.db import models
+from django.db.models import Model, TextChoices
+from django.utils import timezone 
+from phonenumber_field.modelfields import PhoneNumberField 
+from django.utils.translation import gettext_lazy as _ 
+
+from django.core.mail import send_mail, EmailMessage, EmailMultiAlternatives 
+from django.conf import settings 
+from django.template.loader import render_to_string 
+import datetime 
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
 # Create your models here.
