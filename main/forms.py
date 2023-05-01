dab193bb37cc4b8745b74bbb6a798ceadea1e2c0
Veronica Jones <veronicastarr93@gmail.com>
2023-05-01 15:53:55 -0500

    create ContactForm

--- main/forms.py
+++ main/forms.py
@@ -4,9 +4,21 @@
 # IMPORTS
 
 from django import forms 
+from django.forms import ModelForm
 from main.models import *
 from django.contrib.auth import get_user_model
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Forms
+
+class ContactForm(ModelForm):
+    """
+        Define a Contact form to collect information about a contact.
+    """
+    
+    class Meta:
+        model = Contact 
+        fields = ['first_name', 'last_name', 'business', 'email', 'phone_number', 'type', 'message']
+    
+    
