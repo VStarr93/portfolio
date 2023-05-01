6129170f416b3cf82784083010fe315ea53392cc
Veronica Jones <veronicastarr93@gmail.com>
2023-05-01 13:45:49 -0500

    create Contact Model

--- main/models.py
+++ main/models.py
@@ -17,3 +17,39 @@ import datetime
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your models here.
+
+# Create Contact Model
+# main.models.Contact 
+class Contact(Model):
+    """
+        Define a contact model to gather information about a contact.
+    """
+    
+    # Define model methods 
+    
+    # Define model subclasses 
+    class Type(TextChoices):
+        """
+            Define TextChoices for contact types.
+        """
+        JOB = 'JOB', 'Work Request'
+        GENERAL = 'GENERAL', 'General Inquiry'
+    
+    # Define Auto-Generated model fields 
+    id = models.BigAutoField(primary_key=True) # Primary key
+    date = models.DateTimeField(_('Date'), auto_now=True, blank=True, null=True, help_text="The date and time of the contact")
+    type = models.CharField(_('Type'), max_length=7, default=Type.GENERAL, choices=Type.choices, help_text="The type of contact")
+    
+    # Define Boolean model fields 
+    
+    # Define Optional model fields
+    phone_number = PhoneNumberField(_('Phone Number'), blank=True, help_text="The phone number of the contact")
+    business = models.CharField(_('Business'), max_length=50, blank=True, null=True, help_text="The business name of the contact")
+    
+    # Define Required model fields
+    first_name = models.CharField(_('First Name'), max_length=50, help_text="The first name of the contact")
+    last_name = models.CharField(_('Last Name'), max_length=50, help_text="The last name of the contact")
+    email = models.EmailField(_('Email'), help_text="The email address of the contact")
+    message = models.TextField(_('Message'), help_text="The message of the contact")
+    
+    
