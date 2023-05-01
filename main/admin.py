67d73ebd0c819c69e780b025382f5347e54e92fa
Veronica Jones <veronicastarr93@gmail.com>
2023-05-01 15:54:15 -0500

    register Contact Model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# ./main/admin.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
 
+from django.contrib import admin
+from main.models import Contact
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """ This is the admin interface for the Contact Model."""
+    readonly_fields = ['id', 'date']
+        
