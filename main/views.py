68846dd357afd92a93cb7527658443a7d0f88322
Veronica Jones <veronicastarr93@gmail.com>
2023-05-01 15:52:53 -0500

    create contact view

--- main/views.py
+++ main/views.py
@@ -3,9 +3,32 @@
 #-------------------------------------------------------------
 # IMPORTS
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ContactForm
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your views here.
 
+# Contact View 
+def contact_view(request):
+    """
+        This is a Contact Me view.
+    """
+    
+    if request.method == 'GET':
+        form = ContactForm 
+        return render(request, 'main/contact.html', context={'form': form})
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            message = form.errors 
+            context = {
+                'form': ContactForm(request.POST),
+                'message': message,
+            }
+            return render(request, 'main/contact.html', context)
