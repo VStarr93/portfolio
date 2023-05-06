27f97d04ff1a7fd32074697373eb2b830079f8c1
Veronica Jones <veronicastarr93@gmail.com>
2023-05-06 11:23:25 -0500

    add static and media to urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,6 +22,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
@@ -32,7 +34,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('user/', include('crm_user.urls')),
     path('', include('main.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
