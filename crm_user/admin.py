1913a9179ec6407470340a1078d5700aa34fdedc
Veronica Jones <veronicastarr93@gmail.com>
2023-05-09 15:46:23 -0500

    register Address model

--- crm_user/admin.py
+++ crm_user/admin.py
@@ -5,7 +5,7 @@
 
 from django.contrib import admin
 from crm_user.models import User, Customer, Employee, Admin, AdminProfile, CustomerProfile, EmployeeProfile 
-
+from crm_user.models import Address 
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
@@ -18,3 +18,9 @@ admin.site.register(Admin)
 admin.site.register(AdminProfile)
 admin.site.register(CustomerProfile)
 admin.site.register(EmployeeProfile)
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    """ This is the admin interface for the Address model. """
+    readonly_fields = ['id', 'last_modified', 'last_modified_by']
+    
