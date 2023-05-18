d4c126eb3f429fdf27475a14bf305b4286e92d97
Veronica Jones <veronicastarr93@gmail.com>
2023-05-18 15:00:25 -0500

    create test_SimpleAdminCreationForm.py

---
+++ crm_user/tests/forms/test_SimpleAdminCreationForm.py
@@ -0,0 +1,25 @@
+#./crm_user/tests/forms/test_SimpleAdminCreationForm.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.test import TestCase, override_settings
+from crm_user.forms import *
+from crm_user.models import *
+from django.core.exceptions import ValidationError 
+import datetime 
+from django.core.files.uploadedfile import SimpleUploadedFile 
+import shutil, tempfile 
+
+# For Testing Email Services
+from django.core import mail 
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Variables here.
+
+MEDIA_ROOT = tempfile.mkdtemp()
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Simple Admin Creation Form tests here.
