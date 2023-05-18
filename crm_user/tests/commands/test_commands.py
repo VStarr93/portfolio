d01b416f51efcc01b4a252cb633a9ae0336d8759
Veronica Jones <veronicastarr93@gmail.com>
2023-05-18 13:52:41 -0500

    create test_commands.py

---
+++ crm_user/tests/commands/test_commands.py
@@ -0,0 +1,23 @@
+#./crm_user/tests/commands/test_commands.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.test import TestCase
+
+# Management Commands
+from io import StringIO 
+from django.core.management import call_command 
+from django.contrib.auth.models import Group, Permission 
+from django.contrib.contenttypes.models import ContentType 
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Variables here.
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Management Commands tests here.
+
