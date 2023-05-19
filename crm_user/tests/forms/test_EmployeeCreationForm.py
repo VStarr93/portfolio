a00dcc958cfe18fd7885823d0dd2acc967f36932
Veronica Jones <veronicastarr93@gmail.com>
2023-05-19 13:34:25 -0500

    Create a TestCase for Validating EmployeeCreationForm Fields

--- crm_user/tests/forms/test_EmployeeCreationForm.py
+++ crm_user/tests/forms/test_EmployeeCreationForm.py
@@ -15,3 +15,25 @@ from crm_user.forms import EmployeeCreationForm
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your Employee Creation Form tests here.
+
+# Create a TestCase for Validating EmployeeCreationForm Fields
+# crm_user.tests.forms.test_EmployeeCreationForm.FormValTests
+class FormValTests(TestCase):
+    """ Define a TestCase for Validating EmployeeCreationForm Fields """
+    def setUp(self):
+        """ FormVal setUp method to create form data """
+        self.formdata = {
+            'first_name': 'Sara',
+            'middle_name': 'Elizabeth',
+            'last_name': 'Jackson',
+            'email': 'test@example.com',
+            'password1': 'T3st3x@mpl3',
+            'password2': 'T3st3x@mpl3'
+        }
+    
+    def test_type_is_employee(self):
+        """ Define a test to validate that type is always EMPLOYEE """
+        form = EmployeeCreationForm(self.formdata)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.type == 'EMPLOYEE')
+    
