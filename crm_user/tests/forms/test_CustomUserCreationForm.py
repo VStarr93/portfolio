24d1529838844e5b5748c4e011bfff67302f0606
Veronica Jones <veronicastarr93@gmail.com>
2023-05-19 15:03:43 -0500

    Create a TestCase for CustomUserCreationForm Labels

--- crm_user/tests/forms/test_CustomUserCreationForm.py
+++ crm_user/tests/forms/test_CustomUserCreationForm.py
@@ -84,3 +84,54 @@ class FormReqTests(TestCase):
         form = CustomUserCreationForm(self.formdata)
         self.assertFalse(form.is_valid())
     
+# Create a TestCase for CustomUserCreationForm Labels 
+# crm_user.tests.forms.test_CustomUserCreationForm.FormLabelTests 
+class FormLabelTests(TestCase):
+    """ Define a TestCase for CustomUserCreationForm Labels """
+    def setUp(self):
+        """ FormLabel setUp method to create form data """
+        self.formdata = {
+            'first_name': 'Sara',
+            'middle_name': 'Elizabeth',
+            'last_name': 'Jackson',
+            'type': 'CUSTOMER',
+            'email': 'test@example.com',
+            'password1': 'T3st3x@mpl3',
+            'password2': 'T3st3x@mpl3'
+        }
+    
+    def test_user_field_first_name_label(self):
+        """ Define a test for first_name field label """
+        form = CustomUserCreationForm(self.formdata)
+        self.assertTrue(form.fields['first_name'].label == 'First Name')
+        
+    def test_user_field_middle_name_label(self):
+        """ Define a test for middle_name field label """
+        form = CustomUserCreationForm(self.formdata)
+        self.assertTrue(form.fields['middle_name'].label == 'Middle Name')
+        
+    def test_user_field_last_name_label(self):
+        """ Define a test for last_name field label """
+        form = CustomUserCreationForm(self.formdata)
+        self.assertTrue(form.fields['last_name'].label == 'Last Name')
+        
+    def test_user_field_type_label(self):
+        """ Define a test for type field label """
+        form = CustomUserCreationForm(self.formdata)
+        self.assertTrue(form.fields['type'].label == 'Type')
+        
+    def test_user_field_email_label(self):
+        """ Define a test for email field label """
+        form = CustomUserCreationForm(self.formdata)
+        self.assertTrue(form.fields['email'].label == 'Email')
+        
+    def test_user_field_password1_label(self):
+        """ Define a test for password1 field label """
+        form = CustomUserCreationForm(self.formdata)
+        self.assertTrue(form.fields['password1'].label == 'Password')
+        
+    def test_user_field_password2_label(self):
+        """ Define a test for password2 field label """
+        form = CustomUserCreationForm(self.formdata)
+        self.assertTrue(form.fields['password2'].label == 'Password confirmation')
+              
