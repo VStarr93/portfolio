b0caceafb3e73de1162ea9cc1e50d7343b80fe5d
Veronica Jones <veronicastarr93@gmail.com>
2023-05-19 09:53:59 -0500

    Create a TestCase for different user types submitting AddressForm

--- crm_user/tests/forms/test_AddressForm.py
+++ crm_user/tests/forms/test_AddressForm.py
@@ -154,3 +154,46 @@ class FormReqTests(TestCase):
         form = AddressForm(form_data)
         self.assertFalse(form.is_valid())
     
+# Create a TestCase for different user types submitting AddressForm
+# crm_user.tests.forms.test_AddressForm.FormUserTests 
+class FormUserTests(TestCase):
+    """ Define a TestCase for different user types submitting AddressForm """
+    @classmethod
+    def setUpTestData(cls):
+        """ FormUser setUp method to create test users """
+        Customer.objects.create_user(email="customer@example.com", password="t3st13x@mpl3")
+        Admin.objects.create_user(email="admin@example.com", password="t3st13x@mpl3")
+        Employee.objects.create_user(email="employee@example.com", password="t3st13x@mpl3")
+    
+    def setUp(self):
+        """ FormUser setUp method to create test user variables """
+        self.customer = Customer.objects.get(id=1)
+        self.admin = Admin.objects.get(id=2)
+        self.employee = Employee.objects.get(id=3)
+        self.formdata = {
+            'name': 'home',
+            'type': 'RESIDENTIAL',
+            'address_line1': '123 Sara Lane',
+            'city': 'Houston',
+            'state': 'TX',
+            'zip': '77891',
+        }
+    
+    def test_user_customer(self):
+        """ Define a test for user type Customer"""
+        self.formdata['user'] = self.customer 
+        form = AddressForm(self.formdata)
+        self.assertTrue(form.is_valid())
+    
+    def test_user_admin(self):
+        """ Define a test for user type Admin"""
+        self.formdata['user'] = self.admin 
+        form = AddressForm(self.formdata)
+        self.assertTrue(form.is_valid())
+    
+    def test_user_employee(self):
+        """ Define a test for user type Employee"""
+        self.formdata['user'] = self.employee 
+        form = AddressForm(self.formdata)
+        self.assertTrue(form.is_valid())
+    
