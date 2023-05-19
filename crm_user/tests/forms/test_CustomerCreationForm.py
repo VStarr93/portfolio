1abc51de8fe655ccfef238829a3f64b2453e9a42
Veronica Jones <veronicastarr93@gmail.com>
2023-05-19 13:24:07 -0500

    Create a TestCase for Validating CustomerCreationForm Fields

--- crm_user/tests/forms/test_CustomerCreationForm.py
+++ crm_user/tests/forms/test_CustomerCreationForm.py
@@ -23,3 +23,25 @@ MEDIA_ROOT = tempfile.mkdtemp()
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your Customer Creation Form tests here.
+
+# Create a TestCase for Validating CustomerCreationForm Fields
+# crm_user.tests.forms.test_CustomerCreationForm.FormValTests
+class FormValTests(TestCase):
+    """ Define a TestCase for Validating CustomerCreationForm Fields """
+    def setUp(self):
+        """ FormVal setUp method to create form data """
+        self.formdata = {
+            'first_name': 'Sara',
+            'middle_name': 'Elizabeth',
+            'last_name': 'Jackson',
+            'email': 'test@example.com',
+            'password1': 'T3st3x@mpl3',
+            'password2': 'T3st3x@mpl3'
+        }
+    
+    def test_type_is_customer(self):
+        """ Define a test to validate that type is always CUSTOMER """
+        form = CustomerCreationForm(self.formdata)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.type == 'CUSTOMER')
+    
