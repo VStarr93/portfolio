7f87ba0f166803147d4e53c05c7c402d0d642676
Veronica Jones <veronicastarr93@gmail.com>
2023-05-19 10:55:46 -0500

    Create a TestCase for Validating AdminCreationForm Fields

--- crm_user/tests/forms/test_AdminCreationForm.py
+++ crm_user/tests/forms/test_AdminCreationForm.py
@@ -14,3 +14,25 @@ from crm_user.forms import AdminCreationForm
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your Admin Creation Form tests here.
+
+# Create a TestCase for Validating AdminCreationForm Fields
+# crm_user.tests.forms.test_AdminCreationForm.FormValTests 
+class FormValTests(TestCase):
+    """ Define a TestCase for Validating AdminCreationForm Fields """
+    def setUp(self):
+        """ FormVal setUp method to create form data """
+        self.formdata = {
+            'first_name': 'Sara',
+            'middle_name': 'Elizabeth',
+            'last_name': 'Jackson',
+            'email': 'test@example.com',
+            'password1': 'T3st3x@mpl3',
+            'password2': 'T3st3x@mpl3'
+        }
+    
+    def test_type_is_admin(self):
+        """ Define a test to validate that type is always ADMIN """
+        form = AdminCreationForm(self.formdata)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.type == 'ADMIN')
+    
