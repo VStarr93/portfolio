4a82136c316fd0a3c44acea316e027bbce9f950e
Veronica Jones <veronicastarr93@gmail.com>
2023-05-19 13:49:37 -0500

    Create a TestCase for CustomerCustomizeForm Choices Field

--- crm_user/tests/forms/test_CustomerCustomizeForm.py
+++ crm_user/tests/forms/test_CustomerCustomizeForm.py
@@ -16,3 +16,59 @@ from parameterized import parameterized
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your Customer Customize Form tests here.
+
+# Create a TestCase for CustomerCustomizeForm Choices Field
+# crm_user.tests.forms.test_CustomerCustomizeForm.FormChoiceTests 
+class FormChoiceTests(TestCase):
+    """ Define a TestCase for CustomerCustomizeForm Choice Fields """
+    def setUp(self):
+        """ FormReq setUp method to create form data """
+        self.formdata = {
+            'language': 'ENGLISH',
+            'theme': 'GREEN',
+        }
+
+    @parameterized.expand([
+        ('ENGLISH'),
+        ('SPANISH'),
+        ('MANDARIN'),
+        ('HINDI'),
+        ('FRENCH'),
+        ('ARABIC'),
+        ('RUSSIAN'),
+        ('PORTUGUESE'),
+    ])
+    def test_valid_language_choices(self, a):
+        """ Define a test for valid lanuage choices """
+        self.formdata['language'] = a
+        form = CustomerCustomizeForm(self.formdata)
+        self.assertTrue(form.is_valid())
+        
+    def test_invalid_language_choices(self):
+        """ Define a test for invalid language choices """
+        self.formdata['language'] = 'LANGUAGE'
+        form = CustomerCustomizeForm(self.formdata)
+        self.assertFalse(form.is_valid())
+
+    @parameterized.expand([
+        ('RED'),
+        ('BLUE'),
+        ('GREEN'),
+        ('YELLOW'),
+        ('ORANGE'),
+        ('PURPLE'),
+        ('BLACK'),
+        ('WHITE'),
+    ])
+    def test_valid_theme_choices(self, a):
+        """ Define a test for valid theme choices """
+        self.formdata['theme'] = a
+        form = CustomerCustomizeForm(self.formdata)
+        self.assertTrue(form.is_valid())
+        
+    def test_invalid_theme_choices(self):
+        """ Define a test for invalid theme choices """
+        self.formdata['theme'] = 'LAVENDER'
+        form = CustomerCustomizeForm(self.formdata)
+        self.assertFalse(form.is_valid())
+        
