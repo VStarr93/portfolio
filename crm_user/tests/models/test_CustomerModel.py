a2affeb4831810f995e340485dc6b8f6aea94402
Veronica Jones <veronicastarr93@gmail.com>
2023-05-20 11:57:12 -0500

    create test_CustomerModel.py under tests/models/

---
+++ crm_user/tests/models/test_CustomerModel.py
@@ -0,0 +1,23 @@
+#./crm_user/tests/models/test_CustomerModel.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.test import TestCase, override_settings
+from crm_user.models import Customer
+import datetime 
+from django.core.files.uploadedfile import SimpleUploadedFile 
+import shutil, tempfile 
+
+# For Testing Email Services
+from django.core import mail 
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Variables here.
+
+MEDIA_ROOT = tempfile.mkdtemp()
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Customer - PROXY Model tests here.
