96dc4ecc98dd293d96fc5dc6c62d743c28f21bde
Veronica Jones <veronicastarr93@gmail.com>
2023-05-20 11:46:48 -0500

    create test_AddressModel.py under tests/models/

---
+++ crm_user/tests/models/test_AddressModel.py
@@ -0,0 +1,20 @@
+#./crm_user/tests/models/test_AddressModel.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.test import TestCase
+from crm_user.models import Address
+import datetime 
+
+# For Testing Email Services
+from django.core import mail 
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Variables here.
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Address Model tests here.
