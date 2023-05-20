895775c1973b793534c4c96c5b34b677f65cb2be
Veronica Jones <veronicastarr93@gmail.com>
2023-05-20 11:59:30 -0500

    create test_CustomerProfileModel.py under tests/models/

---
+++ crm_user/tests/models/test_CustomerProfileModel.py
@@ -0,0 +1,20 @@
+#./crm_user/tests/models/test_CustomerProfileModel.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.test import TestCase
+from crm_user.models import CustomerProfile
+import datetime 
+
+# For Testing Email Services
+from django.core import mail 
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Variables here.
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your CustomerProfile Model tests here.
