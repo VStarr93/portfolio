e1daf9b6e0dcdc2707a3ea27be72d208f07c50b3
Veronica Jones <veronicastarr93@gmail.com>
2023-05-20 11:55:15 -0500

    create test_AdminProfileModel.py under tests/models/

---
+++ crm_user/tests/models/test_AdminProfileModel.py
@@ -0,0 +1,19 @@
+#./crm_user/tests/models/test_AdminProfileModel.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.test import TestCase
+from crm_user.models import AdminProfile
+import datetime 
+
+# For Testing Email Services
+from django.core import mail 
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Variables here.
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your AdminProfile Model tests here.
