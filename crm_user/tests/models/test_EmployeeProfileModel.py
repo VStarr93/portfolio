fa9a9cfaef10052cb7311a346cb41041d9b33caf
Veronica Jones <veronicastarr93@gmail.com>
2023-05-20 12:04:26 -0500

    create test_EmployeeProfileModel.py under tests/models/

---
+++ crm_user/tests/models/test_EmployeeProfileModel.py
@@ -0,0 +1,20 @@
+#./crm_user/tests/models/test_EmployeeProfileModel.py
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# IMPORTS
+
+from django.test import TestCase
+from crm_user.models import EmployeeProfile
+import datetime 
+
+# For Testing Email Services
+from django.core import mail 
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your Variables here.
+
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+# Create your EmployeeProfile Model tests here.
