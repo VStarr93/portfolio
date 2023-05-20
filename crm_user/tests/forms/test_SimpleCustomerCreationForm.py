b3b552a1d6e134a8993d15edfe76e46d603e0a44
Veronica Jones <veronicastarr93@gmail.com>
2023-05-20 09:26:28 -0500

    remove unnecessary imports

--- crm_user/tests/forms/test_SimpleCustomerCreationForm.py
+++ crm_user/tests/forms/test_SimpleCustomerCreationForm.py
@@ -3,21 +3,12 @@
 #-------------------------------------------------------------
 # IMPORTS
 
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from crm_user.forms import SimpleCustomerCreationForm
-from crm_user.models import *
-from django.core.exceptions import ValidationError 
-import datetime 
-from django.core.files.uploadedfile import SimpleUploadedFile 
-import shutil, tempfile 
 
-# For Testing Email Services
-from django.core import mail 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your Variables here.
-
-MEDIA_ROOT = tempfile.mkdtemp()
 
 
 #-------------------------------------------------------------
