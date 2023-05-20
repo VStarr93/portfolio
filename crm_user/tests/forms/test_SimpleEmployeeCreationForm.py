e19bd7db88ff7a2239f5e1faaba083455ae3455b
Veronica Jones <veronicastarr93@gmail.com>
2023-05-20 09:28:35 -0500

    Create a TestCase for SimpleEmployeeCreationForm Required Fields

--- crm_user/tests/forms/test_SimpleEmployeeCreationForm.py
+++ crm_user/tests/forms/test_SimpleEmployeeCreationForm.py
@@ -14,3 +14,46 @@ from crm_user.forms import SimpleEmployeeCreationForm
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your Simple Employee Creation Form tests here.
+
+# Create a TestCase for SimpleEmployeeCreationForm Required Fields
+# crm_user.tests.forms.test_SimpleEmployeeCreationForm.FormReqTests 
+class FormReqTests(TestCase):
+    """ Define a TestCase for SimpleEmployeeCreationForm Required Fields """
+    def setUp(self):
+        """ FormReq setUp method to create form data """
+        self.formdata = {
+            'first_name': 'Sara',
+            'middle_name': 'Elizabeth',
+            'last_name': 'Jackson',
+            'email': 'test@example.com',
+        }
+    
+    def test_valid_data(self):
+        """ Define a test for valid data """
+        form = SimpleEmployeeCreationForm(self.formdata)
+        self.assertTrue(form.is_valid())
+    
+    def test_no_first_name(self):
+        """ Define a test for no first name """
+        self.formdata['first_name'] = ''
+        form = SimpleEmployeeCreationForm(self.formdata)
+        self.assertTrue(form.is_valid())
+    
+    def test_no_middle_name(self):
+        """ Define a test for no middle name """
+        self.formdata['middle_name'] = ''
+        form = SimpleEmployeeCreationForm(self.formdata)
+        self.assertTrue(form.is_valid())
+    
+    def test_no_last_name(self):
+        """ Define a test for no last_name """
+        self.formdata['last_name'] = ''
+        form = SimpleEmployeeCreationForm(self.formdata)
+        self.assertTrue(form.is_valid())
+    
+    def test_no_email(self):
+        """ Define a test for no email """
+        self.formdata['email'] = ''
+        form = SimpleEmployeeCreationForm(self.formdata)
+        self.assertFalse(form.is_valid())
+    
