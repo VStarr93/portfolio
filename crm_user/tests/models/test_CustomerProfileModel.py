7afbf2c69f418f862ebba04333ed4611740f977d
Veronica Jones <veronicastarr93@gmail.com>
2023-05-22 22:39:05 -0500

    import NOT_PROVIDED

--- crm_user/tests/models/test_CustomerProfileModel.py
+++ crm_user/tests/models/test_CustomerProfileModel.py
@@ -5,7 +5,7 @@
 
 from django.test import TestCase
 from crm_user.models import Customer, CustomerProfile
-import datetime 
+from django.db.models.fields import NOT_PROVIDED
 
 # For Testing Email Services
 from django.core import mail 
