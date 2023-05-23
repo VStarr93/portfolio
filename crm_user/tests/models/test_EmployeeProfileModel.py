43cb7d7144159dbe26cd8b40034844e15bb60fbe
Veronica Jones <veronicastarr93@gmail.com>
2023-05-22 22:51:24 -0500

    Create a TestCase for Employee Profile Labels

--- crm_user/tests/models/test_EmployeeProfileModel.py
+++ crm_user/tests/models/test_EmployeeProfileModel.py
@@ -18,3 +18,59 @@ from django.core import mail
 #-------------------------------------------------------------
 #-------------------------------------------------------------
 # Create your EmployeeProfile Model tests here.
+
+# Create a TestCase for Employee Profile Labels
+# crm_user.tests.models.test_EmployeeProfileModel.LabelTests
+class LabelTests(TestCase):
+    """ Define a TestCase for Employee Profile Model Labels """
+    @classmethod 
+    def setUpTestData(cls):
+        """ Define setUpTestData method for Employee Profile Model Labels """
+        Employee.objects.create_user(
+            email="doe@example.com",
+            first_name="Sara",
+            middle_name="Lee",
+            last_name="Doe",
+            birth_date='1993-04-14',
+            phone_number="+12125556789",
+        )
+        
+    def setUp(self):
+        """ Define setUp method for Employee Profile Model Labels """
+        self.user = EmployeeProfile.objects.get(id=1)
+        
+    def test_id_label(self):
+        """ Test for Employee Profile Model ID Label """
+        field_label = self.user._meta.get_field('id').verbose_name 
+        self.assertEqual(field_label, 'ID')
+        
+    def test_work_id_label(self):
+        """ Test for Employee Profile Model Work ID Label """
+        field_label = self.user._meta.get_field('work_id').verbose_name 
+        self.assertEqual(field_label, 'Work ID')
+        
+    def test_status_label(self):
+        """ Test for Employee Profile Model Status Label """
+        field_label = self.user._meta.get_field('status').verbose_name
+        self.assertEqual(field_label, 'Status')
+        
+    def test_user_label(self):
+        """ Test for Employee Profile Model User Label """
+        field_label = self.user._meta.get_field('user').verbose_name 
+        self.assertEqual(field_label, 'Employee')
+
+    def test_is_manager_label(self):
+        """ Test for Employee Profile Model Is Manager Label """
+        field_label = self.user._meta.get_field('is_manager').verbose_name 
+        self.assertEqual(field_label, 'Is Manager')
+
+    def test_language_label(self):
+        """ Test for Employee Profile Model Language Label """
+        field_label = self.user._meta.get_field('language').verbose_name 
+        self.assertEqual(field_label, 'Language')
+        
+    def test_theme_label(self):
+        """ Test for Employee Profile Model Theme Label """
+        field_label = self.user._meta.get_field('theme').verbose_name
+        self.assertEqual(field_label, 'Theme')
+        
