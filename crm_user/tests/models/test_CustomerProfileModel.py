343c8a86e2dbd15e5eb6d34b8221eb1cb91b633e
Veronica Jones <veronicastarr93@gmail.com>
2023-05-22 22:40:03 -0500

    Create a TestCase for Customer Profile Help Text

--- crm_user/tests/models/test_CustomerProfileModel.py
+++ crm_user/tests/models/test_CustomerProfileModel.py
@@ -149,3 +149,68 @@ class MaxLengthTests(TestCase):
         max_length = self.user._meta.get_field('credit_owed').max_length
         self.assertEqual(max_length, None)
         
+# Create a TestCase for Customer Profile Help Text
+# crm_user.tests.models.test_CustomerProfileModel.HelpTextTests
+class HelpTextTests(TestCase):
+    """ Define a TestCase for Customer Profile Model Help Text """
+    @classmethod 
+    def setUpTestData(cls):
+        """ Define setUpTestData method for Customer Profile Model Help Text """
+        Customer.objects.create_user(
+            email="doe@example.com",
+            first_name="Sara",
+            middle_name="Lee",
+            last_name="Doe",
+            birth_date='1993-04-14',
+            phone_number="+12125556789",
+        )
+        
+    def setUp(self):
+        """ Define setUp method for Customer Profile Model Help Text """
+        self.user = CustomerProfile.objects.get(id=1)
+        
+    def test_id_help_text(self):
+        """ Test for Customer Profile Model ID Help Text """
+        help_text = self.user._meta.get_field('id').help_text 
+        self.assertEqual(help_text, '')
+        
+    def test_acct_no_help_text(self):
+        """ Test for Customer Profile Model Account Number Help Text """
+        help_text = self.user._meta.get_field('acct_no').help_text 
+        self.assertEqual(help_text, "Your customer account number is auto-generated and cannot be changed.")
+        
+    def test_status_help_text(self):
+        """ Test for Customer Profile Model Status Help Text """
+        help_text = self.user._meta.get_field('status').help_text
+        self.assertEqual(help_text, "Your customer status is adjusted based on frequency of jobs.")
+        
+    def test_user_help_text(self):
+        """ Test for Customer Profile Model User Help Text """
+        help_text = self.user._meta.get_field('user').help_text 
+        self.assertEqual(help_text, "The Customer these details/model are associated with.")
+
+    def test_last_job_help_text(self):
+        """ Test for Customer Profile Model Last Job Help Text """
+        help_text = self.user._meta.get_field('last_job').help_text 
+        self.assertEqual(help_text, "The date of which the last job for customer was performed.")
+
+    def test_language_help_text(self):
+        """ Test for Customer Profile Model Language Help Text """
+        help_text = self.user._meta.get_field('language').help_text 
+        self.assertEqual(help_text, "What language do you speak?")
+        
+    def test_theme_help_text(self):
+        """ Test for Customer Profile Model Theme Help Text """
+        help_text = self.user._meta.get_field('theme').help_text
+        self.assertEqual(help_text, "Choose a theme to use.")
+        
+    def test_balance_owed_help_text(self):
+        """ Test For Customer Profile Model Balance Owed Help Text """
+        help_text = self.user._meta.get_field('balance_owed').help_text
+        self.assertEqual(help_text, "The status of the customer's account balance.")
+         
+    def test_credit_owed_help_text(self):
+        """ Test For Customer Profile Model Credit Owed Help Text """
+        help_text = self.user._meta.get_field('credit_owed').help_text
+        self.assertEqual(help_text, "The status of the customer's account credit.")
+        
