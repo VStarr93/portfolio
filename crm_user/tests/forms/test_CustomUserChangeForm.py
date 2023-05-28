4406c05c39a080eedcd32ed980e94f465b4025d6
Veronica Jones <veronicastarr93@gmail.com>
2023-05-27 20:32:05 -0500

    check form validity on each test

--- crm_user/tests/forms/test_CustomUserChangeForm.py
+++ crm_user/tests/forms/test_CustomUserChangeForm.py
@@ -43,36 +43,43 @@ class FormLabelTests(TestCase):
     def test_change_field_first_name_label(self):
         """ Define a test for first_name field label """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['first_name'].label == 'First Name')
         
     def test_change_field_middle_name_label(self):
         """ Define a test for middle_name field label """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['middle_name'].label == 'Middle Name')
         
     def test_change_field_last_name_label(self):
         """ Define a test for last_name field label """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['last_name'].label == 'Last Name')
         
     def test_change_field_email_label(self):
         """ Define a test for email field label """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['email'].label == 'Email')
         
     def test_change_field_birth_date_label(self):
         """ Define a test for birth date field label """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['birth_date'].label == 'Birth Date')
         
     def test_change_field_phone_number_label(self):
         """ Define a test for phone number field label """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['phone_number'].label == 'Phone Number')
     
     def test_change_field_profile_photo_label(self):
         """ Define a test for profile photo field label """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['profile_photo'].label == 'Profile Photo')
               
 # Create a TestCase for CustomUserChangeForm Help Text 
@@ -101,34 +108,41 @@ class FormHelpTests(TestCase):
     def test_change_field_first_name_help_text(self):
         """ Define a test for first_name field help text """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['first_name'].help_text == 'Enter your first name')
         
     def test_change_field_middle_name_help_text(self):
         """ Define a test for middle_name field help text """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['middle_name'].help_text == 'Enter your middle name')
         
     def test_change_field_last_name_help_text(self):
         """ Define a test for last_name field help text """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['last_name'].help_text == 'Enter your last name')
         
     def test_change_field_email_help_text(self):
         """ Define a test for email field help text """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['email'].help_text == 'Enter your email address')
         
     def test_change_field_birth_date_help_text(self):
         """ Define a test for birth date field help text """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['birth_date'].help_text == 'Enter your birth date as yyyy-mm-dd')
         
     def test_change_field_phone_number_help_text(self):
         """ Define a test for phone number field help text """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['phone_number'].help_text == 'Enter a good contact phone number')
     
     def test_change_field_profile_photo_help_text(self):
         """ Define a test for profile photo field help text """
         form = CustomUserChangeForm(self.formdata)
+        self.assertEqual(form.is_valid(), True)
         self.assertTrue(form.fields['profile_photo'].help_text == 'Upload a photo of yourself')
