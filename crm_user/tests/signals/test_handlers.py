abcece20d16e9e3bb1a56bcdb46698cc7aad987a
Veronica Jones <veronicastarr93@gmail.com>
2023-05-27 20:37:16 -0500

    Create a TestCase for Handler Tests

--- crm_user/tests/signals/test_handlers.py
+++ crm_user/tests/signals/test_handlers.py
@@ -19,3 +19,24 @@ from django.core import mail
 #-------------------------------------------------------------
 # Create your Signal tests here.
 
+# Create a TestCase for Handler Tests
+# crm_user.tests.signals.test_handlers.HandlerTests
+class HandlerTests(TestCase):
+    """ Define a TestCase for Signal Handlers """
+    def test_user_type_customer_creates_customer(self):
+        """ Test that creating a user type customer creates a Customer Profile object """
+        User.objects.create_user(email='test@example.com', type='CUSTOMER')
+        self.assertEqual(CustomerProfile.objects.filter(id=1).exists(), True)
+        self.assertEqual(len(mail.outbox), 2)
+        
+    def test_user_type_employee_creates_employee(self):
+        """ Test that creating a user type employee creates an Employee Profile object """
+        User.objects.create_user(email='test@example.com', type='EMPLOYEE')
+        self.assertEqual(EmployeeProfile.objects.filter(id=1).exists(), True)
+        self.assertEqual(len(mail.outbox), 2)
+        
+    def test_user_type_admin_creates_admin(self):
+        """ Test that creating a user type admin creates an Admin Profile object """
+        User.objects.create_user(email='test@example.com', type='ADMIN')
+        self.assertEqual(AdminProfile.objects.filter(id=1).exists(), True)
+        self.assertEqual(len(mail.outbox), 2)
