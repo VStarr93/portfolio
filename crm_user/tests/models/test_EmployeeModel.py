6247c5a764a3ece4a5e0602ee5a40729259963bd
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 13:39:43 -0500

    replace id parameter with email parameter for getting users

--- crm_user/tests/models/test_EmployeeModel.py
+++ crm_user/tests/models/test_EmployeeModel.py
@@ -44,7 +44,7 @@ class LabelTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Labels """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_id_label(self):
         """ Test for Employee Model ID Label """
@@ -120,7 +120,7 @@ class MaxLengthTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Max Lengths """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_id_max_length(self):
         """ Test for Employee Model ID Max Length """
@@ -196,7 +196,7 @@ class HelpTextTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Help Text """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_id_help_text(self):
         """ Test for Employee Model ID Help Text """
@@ -272,7 +272,7 @@ class ReqTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Required Fields """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_id_required(self):
         """ Test that Employee Model ID is not required """
@@ -372,7 +372,7 @@ class DefaultTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Defaults """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_id_default(self):
         """ Test that Employee Model ID has no default value """
@@ -448,7 +448,7 @@ class ChoicesTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Choices """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_id_choices(self):
         """ Test that Employee Model ID has no choices value """
@@ -524,7 +524,7 @@ class ForeignKeyTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Foreign Key Fields """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_type_foreign_key_related_name(self):
         """ Test that Employee Model Type has a related name value """
@@ -563,7 +563,7 @@ class ImageTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Image Fields """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     @classmethod 
     def tearDownClass(cls):
@@ -593,7 +593,7 @@ class MethodTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Methods """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_age_method(self):
         """ Test for Employee Model Age Method """
@@ -654,7 +654,7 @@ class MetaTests(TestCase):
         
     def setUp(self):
         """ Define setUp method for Employee Model Meta """
-        self.user = Employee.objects.get(id=1)
+        self.user = Employee.objects.get(email="doe@example.com")
         
     def test_ordering(self):
         """ Test Employee Model Ordering """
