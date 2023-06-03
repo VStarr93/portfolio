f5c835c475a47a75f6111954d3c087dd428e5e7e
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 12:48:31 -0500

    import login_required

--- crm_user/views.py
+++ crm_user/views.py
@@ -6,6 +6,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from crm_user.forms import *
 from django.apps import apps
 from crm_user.models import Address
