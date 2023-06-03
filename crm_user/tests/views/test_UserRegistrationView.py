89dc0a19cbe6cc6e9f63d5c141ae71f555c83683
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 14:09:29 -0500

    Create a TestCase for User Registration View Get Request with Admin login

--- crm_user/tests/views/test_UserRegistrationView.py
+++ crm_user/tests/views/test_UserRegistrationView.py
@@ -268,3 +268,77 @@ class GetEmployeeLoginTests(TestCase):
         # Check that correct template is rendered
         self.assertTemplateUsed(response, 'crm_user/user_registration.html')
         
+# Create a TestCase for User Registration View Get Request with Admin login
+# crm_user.tests.views.test_UserRegistrationView.GetAdminLoginTests 
+class GetAdminLoginTests(TestCase):
+    """ Define a TestCase for User Registration View Get Request with Admin login """
+    def setUp(self):
+        """ Define setUp for User Registration View Get Request with Admin login """
+        self.password = 'P@ssw0rd3x@mpl3'
+        self.admin = Admin.objects.create_user(email='admin@example.com', password=self.password)
+
+    def test_template_admin_logged_in_create_customer(self):
+        """ Test that User Registration View will render correct template and context for logged in admins who click on customer """
+        # Log in admin 
+        self.client.login(email=self.admin.email, password=self.password)
+        
+        # Create a Get Request with customerBtn
+        response = self.client.get(reverse('crm_user:register'), {'customerBtn': True})
+        
+        # Check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/registration.html')
+        
+        # check that correct template context is rendered
+        self.assertEqual(response.context['form'], CustomerCreationForm)
+
+    def test_template_admin_logged_in_create_employee(self):
+        """ Test that User Registratoin View will render correct template and context for logged in admins who click on employee """
+        # log in admin 
+        self.client.login(email=self.admin.email, password=self.password)
+        
+        # Create a Get Request with empBtn
+        response = self.client.get(reverse('crm_user:register'), {'empBtn': True})
+        
+        # Check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/registration.html')
+        
+        # check that correct template context is rendered
+        self.assertEqual(response.context['form'], SimpleEmployeeCreationForm)
+
+    def test_template_admin_logged_in_create_admin(self):
+        """ Test that User Registration View will render correct template and context for logged in admins who click on admin """
+        # log in admin
+        self.client.login(email=self.admin.email, password=self.password)
+        
+        # Create a Get Request with adminBtn
+        response = self.client.get(reverse('crm_user:register'), {'adminBtn': True})
+        
+        # Check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/registration.html')
+        
+        # check that correct template context is rendered
+        self.assertEqual(response.context['form'], SimpleAdminCreationForm)
+
+    def test_template_user_registration_admin_logged_in(self):
+        """ Test that User Registration View will render correct template for logged in admins """
+        # Log in admin
+        self.client.login(email=self.admin.email, password=self.password)
+        
+        # Create a Get Request
+        response = self.client.get(reverse('crm_user:register'))
+        
+        # Check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/user_registration.html')
+        
