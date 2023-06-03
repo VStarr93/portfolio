0a2edc554859c45ce7ba8d9b353eb79d2c7af990
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 13:56:55 -0500

    add imports

--- crm_user/tests/admin/test_admin.py
+++ crm_user/tests/admin/test_admin.py
@@ -3,6 +3,17 @@
 #-------------------------------------------------------------
 # IMPORTS
 
+from django.test import TestCase, Client, RequestFactory
+from django.urls import reverse 
+
+# Admin Tests 
+from crm_user.admin import CustomUserAdmin
+from crm_user.forms import CustomUserCreationForm
+from crm_user.models import Admin, Employee, Customer, User
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin.sites import AdminSite
+from django.contrib.admin import site
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
