fec289b31e566289bfebf389888220804b59cb10
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 14:21:41 -0500

    Create a TestCase for Profile View Post Errors Request

--- crm_user/tests/views/test_ProfileView.py
+++ crm_user/tests/views/test_ProfileView.py
@@ -467,3 +467,300 @@ class PostTests(TestCase):
         # Check that address was deleted
         self.assertEqual(Address.objects.filter(id=1).exists(), False)
         
+# Create a TestCase for Profile View Post Errors Request
+# crm_user.tests.views.test_ProfileView.PostErrorsTests
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
+class PostErrorsTests(TestCase):
+    """ Define a TestCase for Profile View Post Request """
+    @classmethod 
+    def setUpTestData(cls):
+        # Create password 
+        cls.password = 'P@ssw0rd3x@mpl3'
+        
+        # Create users 
+        cls.admin = Admin.objects.create_user(email='admin@example.com', password=cls.password)
+        cls.employee = Employee.objects.create_user(email='employee@example.com', password=cls.password)
+        cls.customer = Customer.objects.create_user(email='customer@example.com', password=cls.password)
+        
+        # Create Form Data
+        cls.photo = SimpleUploadedFile("test_image.jpg", b"test_content", "image/jpeg")
+        cls.files = {
+            'profile_photo': cls.photo,
+        }
+        long_name = 'hello' * 11
+        cls.changedataerror = {
+            'first_name': long_name,
+            'middle_name': long_name,
+            'last_name': long_name,
+            'email': 'jsmith',
+            'birth_date': '04-14-1993',
+            'phone_number': '2125556890',
+            'profile_photo': SimpleUploadedFile('photo.mp4', b'test_content', 'video'),
+            'submitChange': True,
+        }
+        cls.customizedataerror = {
+            'theme': 'INDIGO',
+            'language': 'EN',
+            'submitCustomize': True,
+        }
+        long_address = long_name * 6
+        cls.addressdataerror = {
+            'form-0-name': '',
+            'form-0-type': '',
+            'form-0-address_line1': '',
+            'form-0-address_line2': long_address,
+            'form-0-city': '',
+            'form-0-state': 'Texas',
+            'form-0-zip': '77891-123',
+            'form-TOTAL_FORMS': 1,
+            'form-INITIAL_FORMS': 0,
+            'form-MAX-NUM_FORMS': 1000,            
+            'form-MIN-NUM_FORMS': 0,            
+            'submitAddress': True,
+        }
+        
+        # Create User Clients
+        cls.aclient = Client()
+        cls.eclient = Client()
+        cls.cclient = Client()
+        
+        # Log in User for each client
+        cls.aclient.login(email=cls.admin.email, password=cls.password)
+        cls.eclient.login(email=cls.employee.email, password=cls.password)
+        cls.cclient.login(email=cls.customer.email, password=cls.password)
+        
+        # Create Post Request for Customize Form 
+        cls.aCustomizeErrorResponse = cls.aclient.post(reverse('crm_user:profile'), cls.customizedataerror)
+        cls.eCustomizeErrorResponse = cls.eclient.post(reverse('crm_user:profile'), cls.customizedataerror)
+        cls.cCustomizeErrorResponse = cls.cclient.post(reverse('crm_user:profile'), cls.customizedataerror)
+        
+        # Create Post Request for Address Form
+        cls.aAddressErrorResponse = cls.aclient.post(reverse('crm_user:profile'), cls.addressdataerror)
+        cls.eAddressErrorResponse = cls.eclient.post(reverse('crm_user:profile'), cls.addressdataerror)
+        cls.cAddressErrorResponse = cls.cclient.post(reverse('crm_user:profile'), cls.addressdataerror)
+        
+        # Create Post Request for Change Form 
+        cls.aChangeErrorResponse = cls.aclient.post(reverse('crm_user:profile'), cls.changedataerror)
+        cls.eChangeErrorResponse = cls.eclient.post(reverse('crm_user:profile'), cls.changedataerror)
+        cls.cChangeErrorResponse = cls.cclient.post(reverse('crm_user:profile'), cls.changedataerror)
+        
+    @classmethod 
+    def tearDownClass(cls):
+        shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+        super().tearDownClass()
+
+    def test_template_on_errors_admin(self):
+        """ Test that Profile View will render the correct template for admin users submitting an invalid form """
+        # Create Response variable
+        response = self.__class__.aAddressErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template is rendered for admin users
+        self.assertTemplateUsed(response, 'crm_user/snippets/profile/admin_acct.html')
+        self.assertTemplateUsed(response, 'crm_user/profile.html')
+    
+    def test_template_on_errors_employee(self):
+        """ Test that Profile View will render the correct template for employee users submitting an invalid form """
+        # Create Response variable
+        response = self.__class__.eAddressErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template is rendered for employee users
+        self.assertTemplateUsed(response, 'crm_user/snippets/profile/emp_acct.html')
+        self.assertTemplateUsed(response, 'crm_user/profile.html')
+    
+    def test_template_on_errors_customer(self):
+        """ Test that Profile View will render the correct template for customer users submitting an invalid form """
+        # Create Response variable
+        response = self.__class__.cAddressErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template is rendered for customer users
+        self.assertTemplateUsed(response, 'crm_user/snippets/profile/cust_acct.html')
+        self.assertTemplateUsed(response, 'crm_user/profile.html')
+    
+    def test_template_context_on_errors_address_form(self):
+        """ Test that Profile View will render the correct template context for submitting an invalid AddressForm """
+        # Create Response variable
+        response = self.__class__.aAddressErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template context is rendered for admin users
+        self.assertIsInstance(response.context['changeForm'], CustomUserChangeForm)
+        self.assertEqual(response.context['changeForm'].instance, self.admin)
+        self.assertIsInstance(response.context['customizeForm'], AdminCustomizeForm)
+        self.assertEqual(response.context['customizeForm'].instance, self.admin.admin_profile)
+        self.assertEqual(response.context['addressFormSet'][0]['name'].value(), self.addressdataerror['form-0-name'])
+        self.assertEqual(response.context['addressFormSet'].queryset, None)
+        self.assertEqual(response.context['addressFormSet'].extra, 1)
+        self.assertEqual(response.context['addressFormSet'].can_delete, True)
+        self.assertEqual(response.context['addressFormSet'].model, Address)
+        self.assertIsInstance(response.context['addressFormSet'].forms[0], AddressForm)
+        self.assertIsInstance(response.context['addressFormHelper'], AddressFormHelper)
+
+    def test_template_context_on_errors_change_form(self):
+        """ Test that Profile View will render the correct template context for submitting an invalid CustomUserChangeForm """
+        # Create Response variable
+        response = self.__class__.eChangeErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template context is rendered for employee users
+        self.assertIsInstance(response.context['changeForm'], CustomUserChangeForm)
+        self.assertEqual(response.context['changeForm'].instance, self.employee)
+        self.assertEqual(response.context['changeForm']['first_name'].value(), self.changedataerror['first_name'])
+        self.assertIsInstance(response.context['customizeForm'], EmployeeCustomizeForm)
+        self.assertEqual(response.context['customizeForm'].instance, self.employee.emp_profile)
+        self.assertQuerysetEqual(response.context['addressFormSet'].queryset, Address.objects.filter(user=self.employee))
+        self.assertEqual(response.context['addressFormSet'].extra, 1)
+        self.assertEqual(response.context['addressFormSet'].can_delete, True)
+        self.assertEqual(response.context['addressFormSet'].model, Address)
+        self.assertIsInstance(response.context['addressFormSet'].forms[0], AddressForm)
+        self.assertIsInstance(response.context['addressFormHelper'], AddressFormHelper)
+
+    def test_template_context_on_errors_admin_customize_form(self):
+        """ Test that Profile View will render the correct template context for admin users submitting an invalid AdminCustomizeForm """
+        # Create Response variable
+        response = self.__class__.aCustomizeErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template context is rendered for customer users
+        self.assertIsInstance(response.context['changeForm'], CustomUserChangeForm)
+        self.assertEqual(response.context['changeForm'].instance, self.admin)
+        self.assertIsInstance(response.context['customizeForm'], AdminCustomizeForm)
+        self.assertEqual(response.context['customizeForm'].instance, self.admin.admin_profile)
+        self.assertEqual(response.context['customizeForm']['language'].value(), self.customizedataerror['language'])
+        self.assertEqual(response.context['customizeForm']['theme'].value(), self.customizedataerror['theme'])
+        self.assertQuerysetEqual(response.context['addressFormSet'].queryset, Address.objects.filter(user=self.admin))
+        self.assertEqual(response.context['addressFormSet'].extra, 1)
+        self.assertEqual(response.context['addressFormSet'].can_delete, True)
+        self.assertEqual(response.context['addressFormSet'].model, Address)
+        self.assertIsInstance(response.context['addressFormSet'].forms[0], AddressForm)
+        self.assertIsInstance(response.context['addressFormHelper'], AddressFormHelper)
+    
+    def test_template_context_on_errors_employee_customize_form(self):
+        """ Test that Profile View will render the correct template context for employee users submitting an invalid EmployeeCustomizeForm """
+        # Create Response variable
+        response = self.__class__.eCustomizeErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template context is rendered for customer users
+        self.assertIsInstance(response.context['changeForm'], CustomUserChangeForm)
+        self.assertEqual(response.context['changeForm'].instance, self.employee)
+        self.assertIsInstance(response.context['customizeForm'], EmployeeCustomizeForm)
+        self.assertEqual(response.context['customizeForm'].instance, self.employee.emp_profile)
+        self.assertEqual(response.context['customizeForm']['language'].value(), self.customizedataerror['language'])
+        self.assertEqual(response.context['customizeForm']['theme'].value(), self.customizedataerror['theme'])
+        self.assertQuerysetEqual(response.context['addressFormSet'].queryset, Address.objects.filter(user=self.employee))
+        self.assertEqual(response.context['addressFormSet'].extra, 1)
+        self.assertEqual(response.context['addressFormSet'].can_delete, True)
+        self.assertEqual(response.context['addressFormSet'].model, Address)
+        self.assertIsInstance(response.context['addressFormSet'].forms[0], AddressForm)
+        self.assertIsInstance(response.context['addressFormHelper'], AddressFormHelper)
+    
+    def test_template_context_on_errors_customer_customize_form(self):
+        """ Test that Profile View will render the correct template context for customer users submitting an invalid CustomerCustomizeForm """
+        # Create Response variable
+        response = self.__class__.cCustomizeErrorResponse
+        
+        # Check that status code is "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template context is rendered for customer users
+        self.assertIsInstance(response.context['changeForm'], CustomUserChangeForm)
+        self.assertEqual(response.context['changeForm'].instance, self.customer)
+        self.assertIsInstance(response.context['customizeForm'], CustomerCustomizeForm)
+        self.assertEqual(response.context['customizeForm'].instance, self.customer.profile)
+        self.assertEqual(response.context['customizeForm']['language'].value(), self.customizedataerror['language'])
+        self.assertEqual(response.context['customizeForm']['theme'].value(), self.customizedataerror['theme'])
+        self.assertQuerysetEqual(response.context['addressFormSet'].queryset, Address.objects.filter(user=self.customer))
+        self.assertEqual(response.context['addressFormSet'].extra, 1)
+        self.assertEqual(response.context['addressFormSet'].can_delete, True)
+        self.assertEqual(response.context['addressFormSet'].model, Address)
+        self.assertIsInstance(response.context['addressFormSet'].forms[0], AddressForm)
+        self.assertIsInstance(response.context['addressFormHelper'], AddressFormHelper)
+
+    def test_form_errors_change_form(self):
+        """ Test that an invalid CustomUserChangeForm submitted by user displays correct form errors """
+        # Create Response variable
+        response = self.__class__.aChangeErrorResponse
+        
+        # Check that response status is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that response displays correct form errors
+        self.assertFormError(response, form='changeForm', field='email', errors='Enter a valid email address.',)
+        self.assertFormError(response, form='changeForm', field='birth_date', errors='Enter a date in the format yyyy-mm-dd.')
+        self.assertFormError(response, form='changeForm', field='phone_number', errors='Enter a valid phone number (e.g. +12125552368).')
+        self.assertFormError(response, form='changeForm', field='profile_photo', errors='Upload a valid image. The file you uploaded was either not an image or a corrupted image.')
+        self.assertFormError(response, form='changeForm', field='first_name', errors='Ensure this value has at most 50 characters (it has 55).')
+        self.assertFormError(response, form='changeForm', field='middle_name', errors='Ensure this value has at most 50 characters (it has 55).')
+        self.assertFormError(response, form='changeForm', field='last_name', errors='Ensure this value has at most 50 characters (it has 55).')
+    
+    def test_form_errors_admin_customize_form(self):
+        """ Test that an invalid AdminCustomizeForm submitted by admin user displays correct form errors """
+        # Create Response variable
+        response = self.__class__.aCustomizeErrorResponse
+        
+        # Check that response status is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that response displays correct form errors
+        self.assertFormError(response, form='customizeForm', field='language', errors='Select a valid choice. EN is not one of the available choices.',)
+        self.assertFormError(response, form='customizeForm', field='theme', errors='Select a valid choice. INDIGO is not one of the available choices.')
+        
+    def test_form_errors_customer_customize_form(self):
+        """ Test that an invalid CustomerCustomizeForm submitted by customer user displays correct form errors """
+        # Create Response variable
+        response = self.__class__.cCustomizeErrorResponse
+        
+        # Check that response status is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that response displays correct form errors
+        self.assertFormError(response, form='customizeForm', field='language', errors='Select a valid choice. EN is not one of the available choices.',)
+        self.assertFormError(response, form='customizeForm', field='theme', errors='Select a valid choice. INDIGO is not one of the available choices.')
+        
+    def test_form_errors_employee_customize_form(self):
+        """ Test that an invalid EmployeeCustomizeForm submitted by employee user displays correct form errors """
+        # Create Response variable
+        response = self.__class__.eCustomizeErrorResponse
+        
+        # Check that response status is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that response displays correct form errors
+        self.assertFormError(response, form='customizeForm', field='language', errors='Select a valid choice. EN is not one of the available choices.',)
+        self.assertFormError(response, form='customizeForm', field='theme', errors='Select a valid choice. INDIGO is not one of the available choices.')
+        
+    def test_form_errors_address_form(self):
+        """ Test that an invalid AddressForm submitted by user displays correct form errors """
+        # Create Response variable
+        response = self.__class__.aAddressErrorResponse
+        
+        # Check that response status is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that response displays correct form errors
+        self.assertFormsetError(response, formset='addressFormSet', form_index=0, field='name', errors='This field is required.',)
+        self.assertFormsetError(response, formset='addressFormSet', form_index=0, field='type', errors='This field is required.',)
+        self.assertFormsetError(response, formset='addressFormSet', form_index=0, field='address_line1', errors='This field is required.',)
+        self.assertFormsetError(response, formset='addressFormSet', form_index=0, field='address_line2', errors='Ensure this value has at most 254 characters (it has 330).',)
+        self.assertFormsetError(response, formset='addressFormSet', form_index=0, field='city', errors='This field is required.')
+        self.assertFormsetError(response, formset='addressFormSet', form_index=0, field='state', errors='Ensure this value has at most 2 characters (it has 5).')
+        self.assertFormsetError(response, formset='addressFormSet', form_index=0, field='zip', errors='Enter a whole number.')
+        
+    
