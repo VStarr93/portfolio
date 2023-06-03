aa876e60b11063dc8bc0ca9e7c6a3b12423b56c3
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 12:39:50 -0500

    Add Receiver for setting permissions on new Admin creation

--- crm_user/signals/handlers.py
+++ crm_user/signals/handlers.py
@@ -248,18 +248,45 @@ def create_admin_profile(sender, **kwargs):
         )
         user.last_modified_by = user 
         user.save()
-        form = PasswordResetForm({'email': new_profile.user.email,})
-        assert form.is_valid()
-        request = HttpRequest()
-        request.META['SERVER_NAME'] = 'localhost'
-        request.META['SERVER_PORT'] = '8000'
-        form.save(
-            request=request, 
-            use_https=False,
-            from_email='veronicastarr93@gmail.com',
-            email_template_name='registration/password_reset_email.html'
-        )
         
+# Add Receiver for setting permissions on new Admin creation
+@receiver(post_save, sender=Admin)
+def admin_set_permissions(sender, **kwargs):
+    """
+    Set object-level permissions. 
+    The admin user can view, change, and delete the object, 
+    The Admin group can view, change, and delete the object,
+    The Employee - Manager group can view the object,
+    """
+    
+    user, created = kwargs["instance"], kwargs["created"]
+    if created and user.email != settings.ANONYMOUS_USER_NAME:      
+        # Get permissions
+        content_type = ContentType.objects.get_for_model(Admin)
+        view_perm = Permission.objects.get(content_type__app_label='crm_user', codename='view_user')  
+        change_perm = Permission.objects.get(content_type__app_label='crm_user', codename='change_user')  
+        delete_perm = Permission.objects.get(content_type__app_label='crm_user', codename='delete_user')  
+        
+        # Create list of all permissions
+        perms = [view_perm, change_perm, delete_perm]
+        
+        # Assign the admin view, delete, and change permission
+        for perm in perms:
+            assign_perm(perm,user,user)
+
+        # Assign Admin Group view, delete, and change permissions
+        group, created = Group.objects.get_or_create(name='Admins')
+        for perm in perms:
+            assign_perm(perm, group, user)
+
+        # Assign Employee - Manager Group view permissions
+        group, created = Group.objects.get_or_create(name='Employee - Manager')
+        assign_perm(view_perm, group, user)
+            
+        # Assign admin user to Admin Group
+        group, created = Group.objects.get_or_create(name='Admins')
+        user.groups.add(group)
+    
 # Add Receiver for creating Profiles from User Model
 @receiver(post_save, sender=User)
 def create_profile(sender, **kwargs):
