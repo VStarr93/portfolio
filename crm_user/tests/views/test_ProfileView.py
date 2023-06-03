bd95b7ba363041960b7a0172fdc2431de0d051b5
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 14:17:34 -0500

    add imports

--- crm_user/tests/views/test_ProfileView.py
+++ crm_user/tests/views/test_ProfileView.py
@@ -3,6 +3,25 @@
 #-------------------------------------------------------------
 # IMPORTS
 
+from django.test import TestCase, override_settings, Client 
+from crm_user.forms import *
+from crm_user.models import *
+from django.core.exceptions import ValidationError 
+import datetime 
+from django.core.files.uploadedfile import SimpleUploadedFile 
+import shutil, tempfile 
+from django.forms.models import modelformset_factory 
+from django.conf import settings  
+
+# For Testing Email Services
+from django.core import mail 
+
+# For HTTP 
+from http import HTTPStatus
+
+# For DB Freeze 
+from freezegun import freeze_time 
+from django.utils import timezone
 
 #-------------------------------------------------------------
 #-------------------------------------------------------------
