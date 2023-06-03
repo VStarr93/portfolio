d8b85ec393b47b1892a9fa265eccf152bd63faf6
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 14:09:13 -0500

    Create a TestCase for User Registration View Get Request with Employee login

--- crm_user/tests/views/test_UserRegistrationView.py
+++ crm_user/tests/views/test_UserRegistrationView.py
@@ -143,3 +143,128 @@ class GetCustomerLoginTests(TestCase):
         # Check that status code is 302
         self.assertEqual(response.status_code, 302)
         
+# Create a TestCase for User Registration View Get Request with Employee login
+# crm_user.tests.views.test_UserRegistrationView.GetEmployeeLoginTests 
+class GetEmployeeLoginTests(TestCase):
+    """ Define a TestCase for User Registration View Get Request with Employee login """
+    def setUp(self):
+        """ Define setUp for User Registration View Get Request with Employee login """
+        self.password = 'P@ssw0rd3x@mpl3'
+        self.empManager = Employee.objects.create_user(email='empmanager@example.com', password=self.password)
+        self.empManager.emp_profile.is_manager = True
+        self.empManager.emp_profile.save()
+        self.employee = Employee.objects.create_user(email='employee@example.com', password=self.password)
+        
+    def test_redirect_employee_logged_in_not_manager_create_employee(self):
+        """ Test that User Registration View will redirect logged in employees that are not managers who click on employee to home page """
+        # Log in employee
+        self.client.login(email=self.employee.email, password=self.password)
+        
+        # Create Get Request wight empBtn
+        response = self.client.get(reverse('crm_user:register'), {'empBtn': True})
+        
+        # Check that status code is 302
+        self.assertEqual(response.status_code, 302)
+        
+    def test_redirect_employee_logged_in_not_manager_create_admin(self):
+        """ Test that User Registration View will redirect logged in employees that are not managers who click on admin to home page """
+        # Log in employee 
+        self.client.login(email=self.employee.email, password=self.password)
+        
+        # Create Get Request with adminBtn 
+        response = self.client.get(reverse('crm_user:register'), {'adminBtn': True})
+        
+        # Check that status code is 302
+        self.assertEqual(response.status_code, 302)
+
+    def test_template_employee_logged_in_manager_create_employee(self):
+        """ Test that User Registration View will render correct template and context for logged in employees that are managers who click on employee """
+        # log in employee as manager
+        self.client.login(email=self.empManager.email, password=self.password)
+        
+        # Check that employee is a manager
+        self.assertEqual(self.empManager.emp_profile.is_manager, True)
+        
+        # Create a Get Request with empBtn 
+        response = self.client.get(reverse('crm_user:register'), {'empBtn': True,})
+        
+        # Check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/registration.html')
+        
+        # check that correct template context is rendered
+        self.assertEqual(response.context['form'], SimpleEmployeeCreationForm)
+
+    def test_template_employee_logged_in_manager_create_admin(self):
+        """ Test that User Registration View will render correct template and context for logged in employees that are managers who click on admin """
+        # Log in employee as manager 
+        self.client.login(email=self.empManager.email, password=self.password)
+        
+        # Check that employee is a manager
+        self.assertEqual(self.empManager.emp_profile.is_manager, True)
+        
+        # Create a Get Request with adminBtn
+        response = self.client.get(reverse('crm_user:register'), {'adminBtn': True})
+        
+        # check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/registration.html')
+        
+        # check that correct template context is rendered
+        self.assertEqual(response.context['form'], SimpleAdminCreationForm)
+
+    def test_template_employee_logged_in_manager_create_customer(self):
+        """ Test that User Registration View will render correct template and context for logged in employees that are managers who click on customer """
+        # Log in employee as manager 
+        self.client.login(email=self.empManager.email, password=self.password)
+        
+        # check that employee is a manager
+        self.assertEqual(self.empManager.emp_profile.is_manager, True)
+        
+        # Create a Get Request with customerBtn
+        response = self.client.get(reverse('crm_user:register'), {'customerBtn': True})
+        
+        # check that status code is 200 
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/registration.html')
+        
+        # check that correct template context is rendered
+        self.assertEqual(response.context['form'], CustomerCreationForm)
+
+    def test_template_employee_logged_in_not_manager_create_customer(self):
+        """ Test that User Registration View will render correct template and context for logged in employees that are not managers who click on customer """
+        # Log in employee 
+        self.client.login(email=self.employee.email, password=self.password)
+        
+        # Create a Get Request with customerBtn
+        response = self.client.get(reverse('crm_user:register'), {'customerBtn': True})
+        
+        # Check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/registration.html')
+        
+        # check that correct template context is rendered
+        self.assertEqual(response.context['form'], CustomerCreationForm)
+
+    def test_template_user_registration_employee_logged_in(self):
+        """ Test that User Registration View will render correct template for logged in employees """
+        #Log in employee 
+        self.client.login(email=self.employee.email, password=self.password)
+        
+        # Create a Get Request
+        response = self.client.get(reverse('crm_user:register'))
+        
+        # Check that status code is 200
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that correct template is rendered
+        self.assertTemplateUsed(response, 'crm_user/user_registration.html')
+        
