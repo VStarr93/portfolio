32526fad9fdf234f2c28211b2f26d72dbf3784c7
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 14:08:43 -0500

    Create a TestCase for User Registration View Get Request with Customer login

--- crm_user/tests/views/test_UserRegistrationView.py
+++ crm_user/tests/views/test_UserRegistrationView.py
@@ -90,3 +90,56 @@ class GetAnonUserTests(TestCase):
         # Check that correct template is rendered
         self.assertTemplateUsed(response, 'crm_user/user_registration.html')
     
+# Create a TestCase for User Registration View Get Request with Customer login
+# crm_user.tests.views.test_UserRegistrationView.GetCustomerLoginTests 
+class GetCustomerLoginTests(TestCase):
+    """ Define a TestCase for User Registration View Get Request with Customer login """
+    def setUp(self):
+        """ Define setUp for User Registration View Get Request with Customer login """
+        self.password = 'P@ssw0rd3x@mpl3'
+        self.customer = Customer.objects.create_user(email='customer@example.com', password=self.password)
+
+    def test_redirect_user_registration_customer_logged_in(self):
+        """ Test that User Registration View will redirect logged in customers to home page """
+        # Log in customer 
+        self.client.login(email=self.customer.email, password=self.password)
+        
+        # Create Get Request 
+        response = self.client.get(reverse('crm_user:register'))
+        
+        # Check that status code is 302
+        self.assertEqual(response.status_code, 302)
+        
+    def test_redirect_customer_logged_in_create_customer(self):
+        """ Test that User Registration View will redirect logged in customers who click on customer to home page """
+        # Log in customer 
+        self.client.login(email=self.customer.email, password=self.password)
+        
+        # Create Get Request with customerBtn
+        response = self.client.get(reverse('crm_user:register'), {'customerBtn': True})
+        
+        # Check that status code is 302
+        self.assertEqual(response.status_code, 302)
+        
+    def test_redirect_customer_logged_in_create_admin(self):
+        """ Test that User Registration View will redirect logged in customers who click on admin to home page """
+        # Log in customer 
+        self.client.login(email=self.customer.email, password=self.password)
+        
+        # Create Get Request with adminBtn
+        response = self.client.get(reverse('crm_user:register'), {'adminBtn': True})
+        
+        # Check that status code is 302
+        self.assertEqual(response.status_code, 302)
+        
+    def test_redirect_customer_logged_in_create_employee(self):
+        """ Test that User Registration View will redirect logged in customers who click on employee to home page """
+        # Log in customer 
+        self.client.login(email=self.customer.email, password=self.password)
+        
+        # Create Get Request with empBtn 
+        response = self.client.get(reverse('crm_user:register'), {'empBtn': True})
+        
+        # Check that status code is 302
+        self.assertEqual(response.status_code, 302)
+        
