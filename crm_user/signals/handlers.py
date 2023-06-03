8df8236166bba53d48067c84d4121b26b9155c80
Veronica Jones <veronicastarr93@gmail.com>
2023-06-03 12:39:12 -0500

    Add Receiver for setting permissions on new employee profile creation

--- crm_user/signals/handlers.py
+++ crm_user/signals/handlers.py
@@ -138,18 +138,68 @@ def create_employee_profile(sender, **kwargs):
         )
         user.last_modified_by = user 
         user.save()
-        form = PasswordResetForm({'email': new_profile.user.email,})
-        assert form.is_valid()
-        request = HttpRequest()
-        request.META['SERVER_NAME'] = 'localhost'
-        request.META['SERVER_PORT'] = '8000'
-        form.save(
-            request=request, 
-            use_https=False,
-            from_email='veronicastarr93@gmail.com',
-            email_template_name='registration/password_reset_email.html'
-        )
+        
+# Add Receiver for setting permissions on new employee profile creation
+@receiver(post_save, sender=EmployeeProfile)
+def employee_profile_set_permissions(sender, **kwargs):
+    """
+    Set object-level permissions. 
+    The employee user can view and change the object, 
+    The Admin group can view, delete, change hire date, change status, and make manager the object,
+    The Employee - Manager group can view, delete, change hire date, change status, and make manager the object,
+    """
+    
+    profile, created = kwargs["instance"], kwargs["created"]
+    user = profile.user
+    if created and user.email != settings.ANONYMOUS_USER_NAME:      
+        # Get permissions
+        content_type = ContentType.objects.get_for_model(EmployeeProfile)
+        view_perm = Permission.objects.get(content_type__app_label='crm_user', codename='view_employeeprofile')  
+        change_perm = Permission.objects.get(content_type__app_label='crm_user', codename='change_employeeprofile')  
+        delete_perm = Permission.objects.get(content_type__app_label='crm_user', codename='delete_employeeprofile')  
+        change_hire_date_perm = Permission.objects.get(content_type__app_label='crm_user', codename='change_employee_hire_date')
+        change_status_perm = Permission.objects.get(content_type__app_label='crm_user', codename='change_employee_status')
+        make_manager_perm = Permission.objects.get(content_type__app_label='crm_user', codename='make_employee_manager')
+        
+        # Assign the employee view and change permission
+        assign_perm(view_perm, user, profile)
+        assign_perm(change_perm, user, profile)
 
+        # Assign Admin Group view, delete, change hire date, change status, and make manager permissions
+        group, created = Group.objects.get_or_create(name='Admins')
+        for perm in [view_perm, delete_perm, change_hire_date_perm, change_status_perm, make_manager_perm]:
+            assign_perm(perm, group, profile)
+
+        # Assign Employee - Manager Group view, delete, change hire date, change status, and make manager permissions
+        group, created = Group.objects.get_or_create(name='Employee - Manager')
+        for perm in [view_perm, delete_perm, change_hire_date_perm, change_status_perm, make_manager_perm]:
+            assign_perm(perm, group, profile)
+    
+    if not created:
+        manager = profile.is_manager 
+        if manager:
+            # check if employee is in manager group
+            if not user.groups.filter(name='Employee - Manager').exists():
+                group, created = Group.objects.get_or_create(name='Employee - Manager')
+                user.groups.add(group)
+                
+            # check if employee is in standard group
+            if user.groups.filter(name='Employee - Standard').exists():
+                # remove standard group
+                group, created = Group.objects.get_or_create(name='Employee - Standard')
+                user.groups.remove(group)
+            
+        else:
+            # check if employee is in manager group
+            if user.groups.filter(name='Employee - Manager').exists():
+                group, created = Group.objects.get_or_create(name='Employee - Manager')
+                user.groups.remove(group)
+            
+            # check if employee is in standard group
+            if not user.groups.filter(name='Employee - Standard').exists():
+                group, created = Group.objects.get_or_create(name='Employee - Standard')
+                user.groups.add(group)
+                
 # Add Receiver for creating Admin Profile from Admin Model
 @receiver(post_save, sender=Admin)
 def create_admin_profile(sender, **kwargs):
